57d67a8cb740a1f05a7d3b59d11c5841c94d658a
khanhdev2503 <binhkhanh25031999@gmail.com>
2023-07-01 02:21:11 +0700

    Tạo layout khóa học, video

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -4,19 +4,22 @@ import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { loginApi } from "../services/authentication/auth";
 import { bannerApi } from "../services/banners";
 import { courseApi } from "../services/courses";
+import { videosApi } from "../services/videos";
 
 export const store = configureStore({
   reducer: {
     [loginApi.reducerPath]: loginApi.reducer,
     [bannerApi.reducerPath]: bannerApi.reducer,
-    [courseApi.reducerPath]: courseApi.reducer
+    [courseApi.reducerPath]: courseApi.reducer,
+    [videosApi.reducerPath]: videosApi.reducer,
   },
 
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       loginApi.middleware,
       bannerApi.middleware,
-      courseApi.middleware
+      courseApi.middleware,
+      videosApi.middleware
     )
 });
 setupListeners(store.dispatch);
