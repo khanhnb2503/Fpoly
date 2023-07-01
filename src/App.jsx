cc8c3d1290944d765f3efa0244ad984ef0b73c68
khanhdev2503 <binhkhanh25031999@gmail.com>
2023-07-01 23:22:54 +0700

    Chỉnh sửa layout chi tiết khóa học

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { Route, Routes } from "react-router-dom";
-import Lessons from "./components/shared/Lessons";
-import { RoutesConstant } from "./routes";
-import Login from './views/auth-views/components/Login';
-import Register from './views/auth-views/components/Register';
+
+import LayoutPublic from './components/layouts';
+// import Lessons from "./components/shared/Lessons";
+// import { RoutesConstant } from "./routes";
+import CoursePage from './views/app-views/course';
 import DetailCoursePage from './views/app-views/detailCourse/index.jsx';
+import HomePage from './views/app-views/home';
+// import Login from './views/auth-views/components/Login';
+// import Register from './views/auth-views/components/Register';
 
 function App() {
   return (
     <>
-      {/* <LayoutPublic>
+      <LayoutPublic>
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/khoa-hoc' element={<CoursePage />} />
-          <Route path='/thong-tin-khoa-hoc' element={ <DetailCoursePage />} />
+          <Route path='/thong-tin-khoa-hoc' element={<DetailCoursePage />} />
         </Routes>
-      </LayoutPublic> */}
-      <Routes>
+      </LayoutPublic>
+      {/* <Routes>
         <Route path='/lessons' element={<Lessons />} />
         <Route path={RoutesConstant.LOGIN} element={<Login />} />
         <Route path={RoutesConstant.REGISTER} element={<Register />} />
-      </Routes>
+      </Routes> */}
     </>
   )
 }
