e504aaac242397dd521dc783480d4b5e98d2fb44
khanhdev2503 <binhkhanh25031999@gmail.com>
2023-07-07 23:11:10 +0700

    Chỉnh sửa lại tên component default layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
+import { Fragment } from 'react';
 import { Route, Routes } from "react-router-dom";
 import LayoutPublic from './components/layouts';
-import { Defaulayout } from './routes';
-import { Fragment } from 'react';
+import { DefaultLayout } from './routes';
 
 function App() {
   return (
     <>
-        <Routes>
-          {Defaulayout.map((route, index) => {
-            const Page = route.component
-            const Layout = route.layout === null ? Fragment  : LayoutPublic
-            return <Route
-              key={index}
-              path={route.path}
-              element={
+      <Routes>
+        {DefaultLayout.map((route, index) => {
+          const Page = route.component
+          const Layout = route.layout === null ? Fragment : LayoutPublic
+          return <Route
+            key={index}
+            path={route.path}
+            element={
               <Layout>
                 <Page />
               </Layout>
             } />
-          })}
-        </Routes>
+        })}
+      </Routes>
     </>
   )
 }
