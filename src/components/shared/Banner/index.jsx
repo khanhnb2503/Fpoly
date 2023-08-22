62fa4b46d5ed3149279968c4057ce64184f0a6c7
khanhdev2503 <binhkhanh25031999@gmail.com>
2023-08-23 00:42:33 +0700

    Fixbug ui profile

--- src/components/shared/Banner/index.jsx
+++ src/components/shared/Banner/index.jsx
@@ -1,7 +1,6 @@
 import { Col, Row } from 'antd';
 import Carousel from 'nuka-carousel';
-import { Link } from 'react-router-dom';
-import BannerBackground from '../../../../public/images/background1.jpg'
+import BannerBackground from '../../../../public/images/background1.jpg';
 
 import { useGetAllBannerQuery } from '../../../services/banners';
 
@@ -17,7 +16,6 @@ function Banner() {
           speed={1000}
           autoplayInterval={5000}
         >
-          {/* {isSuccess && banners.data.map((slide) => ( */}
           <Row key={1} justify='space-between'>
             <Col xl={10} className='slide-show-left'>
               <h3>REACT - C# - PHP</h3>
@@ -26,7 +24,7 @@ function Banner() {
                 Tạo nên sản phẩm, mang lại giá trị cho cộng đồng.
               </p>
             </Col>
-            <Col xl={14} className='slide-show-right'>
+            <Col xl={12} className='slide-show-right'>
               <Row justify='end'>
                 <Col xl={24}>
                   <img className='image-background' src={BannerBackground} />
@@ -34,7 +32,6 @@ function Banner() {
               </Row>
             </Col>
           </Row>
-          {/* ))} */}
         </Carousel>
       </div>
     </div>
