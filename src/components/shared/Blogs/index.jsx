78cd44cfe029389978a8ab4511dab3bf68a84a33
trangdev1999 <tranghant2006@gmail.com>
2023-08-22 21:35:58 +0700

    Fixbbug ui avatar forums

--- src/components/shared/Blogs/index.jsx
+++ src/components/shared/Blogs/index.jsx
@@ -1,32 +1,66 @@
 import { Card, Col, Row, Tag } from "antd";
 import moment from "moment";
-import { ImHistory } from "react-icons/im";
+import { ImHistory, ImTelegram } from "react-icons/im";
 import { Link } from "react-router-dom";
 import { useAddViewPostMutation, useGetPostsTrendingQuery } from '../../../services/forum';
+
+import AvatarForum1 from '../../../../public/images/blog 11.png';
+import AvatarForum2 from '../../../../public/images/blog 12.png';
+import AvatarForum3 from '../../../../public/images/blog 13.png';
+import AvatarForum4 from '../../../../public/images/blog 14.png';
+import AvatarForum5 from '../../../../public/images/blog 9.png';
+import { useEffect, useState } from "react";
+
 
 const { Meta } = Card;
 
 function Blogs() {
 	const { data: forums, isSuccess } = useGetPostsTrendingQuery();
-	const [addViewPost] = useAddViewPostMutation()
+	const [addViewPost] = useAddViewPostMutation();
+	const [filterForum, setFilterForum] = useState([]);
 
 	const handleAddView = (id) => {
 		const { data } = addViewPost({ post_id: id })
-	}
+	};
+
+	const listArrayForum = [
+		{ id: 9, image: AvatarForum1 },
+		{ id: 12, image: AvatarForum2 },
+		{ id: 11, image: AvatarForum3 },
+		{ id: 13, image: AvatarForum4 },
+		{ id: 14, image: AvatarForum5 }
+	];
+
+	useEffect(() => {
+		if(forums) {
+			forums.map((forum) => {
+				let arr;
+				listArrayForum.map((item) => {
+					if(forum.id == item.id) {
+						arr = {id: forum.id, title: forum.title, image: item.image, user: forum.user}
+					}
+				});
+				setFilterForum((state) => [...state, arr]);
+			})
+		}
+	}, [isSuccess]);
+
+	console.log(filterForum)
+
 
 	return (
 		<div className="wrapper__blogs">
 			{isSuccess && (
 				<div className="blogs">
 					<Row justify='start' align='middle' gutter={[50, 45]}>
-						{isSuccess > 0 && forums.map((item) => (
+						{isSuccess > 0 && filterForum.map((item) => (
 							<Col key={item.id} xl={6} className="blog-item">
 								<Card
 									hoverable
 									cover={
 										<img
 											alt="example"
-											src={`/public/images/blog ${item.id}.jpg`}
+											src={item.image}
 										/>
 									}
 									className="item-forums"
